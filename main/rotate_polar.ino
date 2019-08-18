2e5624e784cd8dde4aa59d2162f8347fd6f3e9e7
Ben Delarre <ben@delarre.net>
2019-08-18 09:26:50 -0700

    Adds FastLED support

--- main/rotate_polar.ino
+++ main/rotate_polar.ino
@@ -12,7 +12,7 @@ void ShiftPolar(const std::vector<int>& colors,
     const int color_index =
       (int) (color_shift + kStripPolarPositions[led_strip] * colors_len) % colors_len;
     for (int led_index = 0; led_index < kVerticalStripLen; led_index++) {
-      leds.setPixel(led_index + led_offset_vertical, colors[color_index]);
+      leds[led_index + led_offset_vertical] = colors[color_index];
     }
   }
   // Set color for the mid strip.
@@ -21,7 +21,7 @@ void ShiftPolar(const std::vector<int>& colors,
   for (int led_index = 0; led_index < kMidStripLen; led_index++) {
     const int color_index =
       (int) (color_shift + led_index * proj_factor_mid) % colors_len;
-    leds.setPixel(led_index + led_offset_mid, colors[color_index]);
+    leds[led_index + (int)led_offset_mid] = colors[color_index];
   }
   // Set color for the top strip.
   const double proj_factor_top = colors_len / kTopStripLen;
@@ -29,7 +29,7 @@ void ShiftPolar(const std::vector<int>& colors,
   for (int led_index = 0; led_index < kTopStripLen; led_index++) {
     const int color_index =
       (int) (color_shift + led_index * proj_factor_top) % colors_len;
-    leds.setPixel(led_index + led_offset_top, colors[color_index]);
+    leds[led_index + (int)led_offset_top] = colors[color_index];
   }
 }
 
@@ -44,15 +44,15 @@ void RotatePolar(const double rotate_freq,
     for (double color_shift = 0; color_shift < colors_len;
         color_shift += color_delta) {
       ShiftPolar(colors, color_shift);
-      leds.show();
-      delayMicroseconds(1000000 / refresh_freq);
+      FastLED.show();
+      FastLED.delay(1000 / refresh_freq);
     }
   } else {
     for (double color_shift = colors_len; color_shift > 0;
         color_shift -= color_delta) {
       ShiftPolar(colors, color_shift);
-      leds.show();
-      delayMicroseconds(1000000 / refresh_freq);
+      FastLED.show();
+      FastLED.delay(1000 / refresh_freq);
     }
   }
 }
